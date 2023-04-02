c7687cc4d6fffec8daa9243b56e84b3fc3237a63
Marques Ivory <Maivory12@icloud.com>
2023-04-02 14:28:32 -0400

    importing api for the index.js routes and added /users and /thoughts to the https

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+// imports your apis for the index.js routes in the api folder 
+const apiRoutes = require ('./api');
+
+router.use('/api', apiRoutes);
+router.use((req, res) => {
+    // Error Message 
+    res.status(404).send("<h1> 404 Error <h1>");
+});
+
+module.exports = router;
